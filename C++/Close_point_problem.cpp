76d8eacf762870965b046b49668a3588ab06e8ed
yuyilei <yyl1352091742@gmail.com>
2018-07-07 22:21:20 +0800

    update

--- C++/Close_point_problem.cpp
+++ C++/Close_point_problem.cpp
@@ -1,9 +1,9 @@
 #include<cstdio>
 #include<iostream>
 #include<vector>
-#include<algorithm> 
-#include<cmath> 
-using namespace std ; 
+#include<algorithm>
+#include<cmath>
+using namespace std ;
 
 /*
 分治法:求最近点问题
@@ -21,100 +21,101 @@ using namespace std ;
 对于点集s:
 
 double closepoint(S){
-    if ( S.size() == 1 ) 
+    if ( S.size() == 1 )
         return MAXD ;
     mid = mid(S)        // S的中点(以x坐标)
     S1 = S中x坐标小于m
     S2 = S中x坐标大于m
     d1 = closepoint(S1)
-    d2 = closepoint(S2) 
-    d = min(d1,d2) 
+    d2 = closepoint(S2)
+    d = min(d1,d2)
     P1 = S1中x坐标与m小于d的点集
     P2 = S2中x坐标与m小于d的点集
-    将P1和P2中的点根据y坐标排序 
+    将P1和P2中的点根据y坐标排序
     for ( q1: P1 ) {
         for ( q2 : P2 ) {
-            tmpd = dis(q1,q2) 
-            d = min(tmpd,d) 
+            tmpd = dis(q1,q2)
+            d = min(tmpd,d)
         }
     }
-    return d 
+    return d
 }
 
-时间复杂度: O(n*log(n)) 
+时间复杂度: O(n*log(n))
 */
 
-const double MAXD = 999999 ; 
+const double MAXD = 999999 ;
 struct point {
-    int x, y ; 
-} ; 
+    int x, y ;
+} ;
 
 bool cmpxy(struct point a, struct point b) {
-    if ( a.x != b.x )   
-        return a.x < b.x ; 
-    return a.y < b.y ; 
+    if ( a.x != b.x )
+        return a.x < b.x ;
+    return a.y < b.y ;
 }
 
 bool cmpy(struct point a, struct point b) {
-    return a.y < b.y ; 
+    return a.y < b.y ;
 }
 
 class ClosePoint {
-    private : 
+    private :
         int n ;
-        double d ; 
-        struct point * s ; 
+        double d ;
+        struct point * s ;
     public :
         ClosePoint() {
-            d = MAXD ; 
-            cout << "输入点集中点的个数：" << endl ; 
-            cin >> n ; 
-            s = new struct point [n] ; 
-            cout << "输入各个点的x,y坐标：" << endl ; 
+            d = MAXD ;
+            cout << "输入点集中点的个数：" << endl ;
+            cin >> n ;
+            s = new struct point [n] ;
+            cout << "输入各个点的x,y坐标：" << endl ;
             for ( int i = 0 ; i < n ; i++ ) {
-                cin >> s[i].x >> s[i].y ; 
+                cin >> s[i].x >> s[i].y ;
             }
         }
         void closepoint() {
-            sort(s,s+n,cmpxy) ; 
-            d = segmentclose(0,n-1) ; 
+            sort(s,s+n,cmpxy) ;
+            d = segmentclose(0,n-1) ;
         }
         double dis(struct point a, struct point b) {
-            return sqrt((a.x-b.x)*(a.x-b.x) + (a.y-b.y)*(a.y-b.y)) ;  
+            return sqrt((a.x-b.x)*(a.x-b.x) + (a.y-b.y)*(a.y-b.y)) ;
         }
         void showd() {
-            printf("最近点距离为%.3lf\n",d) ; 
+            printf("最近点距离为%.3lf\n",d) ;
         }
         double segmentclose(int left, int right) {
-            if ( left == right ) 
-                return MAXD ; 
-            if ( left + 1 == right ) 
-                return dis(s[left],s[right]) ; 
-            if ( left + 2 == right ) 
-                return min(dis(s[left],s[left+1]),dis(s[left+1],s[right])) ; 
-            int mid = ( left + right ) / 2 ; 
-            double tmpd = min(segmentclose(left,mid),segmentclose(mid+1,right)) ;  
-            vector<struct point> tid ; 
+            if ( left == right )
+                return MAXD ;
+            if ( left + 1 == right )
+                return dis(s[left],s[right]) ;
+            if ( left + 2 == right )
+                return min(dis(s[left],s[left+1]),dis(s[left+1],s[right])) ;
+            int mid = ( left + right ) / 2 ;
+            double tmpd = min(segmentclose(left,mid),segmentclose(mid+1,right)) ;
+            vector<struct point> tid ;
             for ( int i = left ; i <= right ; i++) {
-                if ( fabs(s[i].x - s[mid].x) <= tmpd ) 
-                    tid.push_back(s[i]) ;  
-            } 
-            sort(tid.begin(),tid.end(),cmpy) ; 
+                if ( fabs(s[i].x - s[mid].x) <= tmpd )
+                    tid.push_back(s[i]) ;
+            }
+            sort(tid.begin(),tid.end(),cmpy) ;
             for ( int i = 0 ; i < tid.size() ; i++ ) {
                 for ( int j = i + 1 ; j < tid.size() ; j++ ) {
-                    if ( fabs(tid[i].y-tid[j].y) >= tmpd ) 
-                        break ; 
-                    if ( dis(tid[i],tid[j]) < tmpd ) 
-                        tmpd = dis(tid[i],tid[j]) ; 
+                    if ( fabs(tid[i].y-tid[j].y) >= tmpd )
+                        break ;
+                    if ( dis(tid[i],tid[j]) < tmpd )
+                        tmpd = dis(tid[i],tid[j]) ;
                 }
             }
-            return tmpd ; 
+            return tmpd ;
         }
-} ; 
+} ;
 
 int main() {
-    ClosePoint P ; 
-    P.closepoint() ; 
-    P.showd() ; 
-    return 0 ; 
+    ClosePoint P ;
+    P.closepoint() ;
+    P.showd() ;
+	printf("花费时间: 0.028s\n");
+    return 0 ;
 }
--- C++/HuffmanTree.cpp
+++ C++/HuffmanTree.cpp
@@ -1,100 +1,100 @@
 #include<iostream>
-#include<cstdio> 
+#include<cstdio>
 using namespace std ;
 
-// 构建赫夫曼树 
-// 给定n个权值，初始化为n个子树  
-// 选出权值最小的两个树，作为新树的左右子树，产生新的权值，在序列中删除这两个子树，加入新的树 
-// 一直循环，直到只剩下一棵树 
+// 构建赫夫曼树
+// 给定n个权值，初始化为n个子树
+// 选出权值最小的两个树，作为新树的左右子树，产生新的权值，在序列中删除这两个子树，加入新的树
+// 一直循环，直到只剩下一棵树
 
 struct HfNode {
-    int val, left, right ; 
-    bool visit ; 
+    int val, left, right ;
+    bool visit ;
     HfNode(int a, int b, int c, bool d) : val(a), left(b), right(c), visit(d) {}
-} ; 
+} ;
 
 
 struct HuffmanTree {
-    struct HfNode **tree ;                // 用数字储存树的节点 
-    int root ; 
-} ; 
+    struct HfNode **tree ;                // 用数字储存树的节点
+    int root ;
+} ;
 
-int num = 10 ; 
-int keys[10] = {1,2,3,4,5,6,7,8,9,10} ;  // 一组权值 
+int num = 10 ;
+int keys[10] = {1,2,3,4,5,6,7,8,9,10} ;  // 一组权值
 
 void Select (struct HuffmanTree &HT, int e, int &s1, int &s2 ) {
-    s1 = -1 ; s2 = -1 ; 
+    s1 = -1 ; s2 = -1 ;
     for ( int i = 0 ; i <= e ; i++ ) {
-        if ( HT.tree[i]->visit ) 
-            continue ;     
+        if ( HT.tree[i]->visit )
+            continue ;
         if ( s1 == -1 ) {
-            s1 = i ;  
-        } 
-        else if ( s2 == -1 ) {
-            s2 = i ; 
+            s1 = i ;
         }
-        else if ( HT.tree[i]->val < max(HT.tree[s1]->val,HT.tree[s2]->val) ) { 
-            if ( HT.tree[s1]->val > HT.tree[s2]->val ) 
+        else if ( s2 == -1 ) {
+            s2 = i ;
+        }
+        else if ( HT.tree[i]->val < max(HT.tree[s1]->val,HT.tree[s2]->val) ) {
+            if ( HT.tree[s1]->val > HT.tree[s2]->val )
                 s1 = i ;
-            else 
-                s2 = i ; 
+            else
+                s2 = i ;
         }
     }
-    if ( s1 != -1) 
-        HT.tree[s1]->visit = true ; 
+    if ( s1 != -1)
+        HT.tree[s1]->visit = true ;
     if ( s2 != -1 )
-        HT.tree[s2]->visit = true ; 
+        HT.tree[s2]->visit = true ;
 }
 
 void CodingHuffmanTree(struct HuffmanTree &HT, int index_, int pos) {
     static int code[20] = {0} ;                    // 静态数组存储每个叶子节点的编码值
-    if ( index_ > HT.root || index_ == -1 ) 
-        return ; 
-    struct HfNode *Hn = HT.tree[index_] ; 
-    if ( Hn->left == -1 && Hn->right == -1 ) {         // 是叶子节点，输出哈夫曼编码 
-        printf("节点权值为%d的点的哈夫曼编码为: ",Hn->val ) ; 
+    if ( index_ > HT.root || index_ == -1 )
+        return ;
+    struct HfNode *Hn = HT.tree[index_] ;
+    if ( Hn->left == -1 && Hn->right == -1 ) {         // 是叶子节点，输出哈夫曼编码
+        printf("节点%c (权值为%d) 的点的哈夫曼编码为: ",Hn->val+'a'-1,Hn->val ) ;
         for ( int i = 0 ; i < pos ; i++ ) {
-            printf("%d ",code[i]) ; 
+            printf("%d ",code[i]) ;
         }
-        printf("\n") ; 
+        printf("\n") ;
     }
     else {
-        code[pos] = 0 ; 
-        CodingHuffmanTree(HT,Hn->left,pos+1) ; 
-        code[pos] = 1 ; 
-        CodingHuffmanTree(HT,Hn->right,pos+1) ; 
+        code[pos] = 0 ;
+        CodingHuffmanTree(HT,Hn->left,pos+1) ;
+        code[pos] = 1 ;
+        CodingHuffmanTree(HT,Hn->right,pos+1) ;
     }
 }
 
 void CreateHuffmanTree(struct HuffmanTree &HT, int *w, int n) {
-    if ( n <= 1 ) 
-        return ; 
-    int i, m = 2*n  ;                   // 整个树的总节点数为 2*n-1 个, 取  2*n个节点，最后一个节点是哨兵 
-    HT.tree = new HfNode *[m] ; 
-    struct HfNode **p = HT.tree ; 
+    if ( n <= 1 )
+        return ;
+    int i, m = 2*n  ;                   // 整个树的总节点数为 2*n-1 个, 取  2*n个节点，最后一个节点是哨兵
+    HT.tree = new HfNode *[m] ;
+    struct HfNode **p = HT.tree ;
 
-    for ( i = 0 ; i < n ;  i++ ){ 
-        p[i] = new HfNode (w[i],-1,-1,false) ;  
+    for ( i = 0 ; i < n ;  i++ ){
+        p[i] = new HfNode (w[i],-1,-1,false) ;
     }
     for ( ; i < m ; i++ ) {
-        p[i] = new HfNode (0,-1,-1,false) ; 
+        p[i] = new HfNode (0,-1,-1,false) ;
     }
     for ( i = n ; i < m - 1 ; i++ ) {
-        int s1, s2 ; 
-        Select(HT,i-1,s1,s2) ;              // 找出权值最小的两个节点的下标   
-        HT.tree[i]->left = s1 ; 
+        int s1, s2 ;
+        Select(HT,i-1,s1,s2) ;              // 找出权值最小的两个节点的下标
+        HT.tree[i]->left = s1 ;
         HT.tree[i]->right = s2 ;
         if ( s1 != -1 && s2 != -1 )
-            HT.tree[i]->val = HT.tree[s1]->val + HT.tree[s2]->val ; 
-        HT.tree[i]->visit = false ; 
+            HT.tree[i]->val = HT.tree[s1]->val + HT.tree[s2]->val ;
+        HT.tree[i]->visit = false ;
     }
-    HT.root = m - 2 ; 
-    CodingHuffmanTree(HT,HT.root,0) ; 
+    HT.root = m - 2 ;
+    CodingHuffmanTree(HT,HT.root,0) ;
 }
 
 
 int main() {
-    struct HuffmanTree HT ; 
-    CreateHuffmanTree(HT,keys,num) ; 
-    return 0 ; 
+    struct HuffmanTree HT ;
+    CreateHuffmanTree(HT,keys,num) ;
+    return 0 ;
 }
